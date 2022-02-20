46ab1a4ddf83b42603d157a3a385d3eceffafabc
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-19 19:46:39 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Harold Hopson on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
