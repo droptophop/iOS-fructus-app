6035c4b0c3828fc618c20af90591474257c9ed51
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-23 18:25:55 -0600

    Adds Fruit row view

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Harold Hopson on 2/23/22.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView()
+    }
+}
