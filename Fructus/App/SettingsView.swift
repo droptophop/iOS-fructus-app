d7838ea0273fb5e15dfa3a009766c2f92225baf1
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-26 13:15:32 -0600

    Adds setting row view to settings view

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -37,8 +37,26 @@ struct SettingsView: View {
                                 .font(.footnote)
                         }
                     }
+                    
                     // MARK: - SECTION 2
+                    
                     // MARK: - SECTION 3
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelVIew(labelText: "Application", labelImage: "iphone")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        SettingsRowView(name: "Developer", content: "Harry Hopson")
+                        SettingsRowView(name: "Designer", content: "Robert Petra")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "Hopson Ventures", linkDestination: "gohopventure.com")
+                        SettingsRowView(name: "Twitter", linkLabel: "@hopsonventures", linkDestination: "twitter.com/hopsonventures")
+                        SettingsRowView(name: SwiftUI, content: "2.0")
+                        SettingsRowView(name: Version, content: "1.1.0")
+                    }
+                    
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(
